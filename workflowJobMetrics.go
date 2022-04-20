7a1a6ecd1851ad50bde99b5800f0d691fff59488
Alain Hélaïli <alain@carbonhub.io>
2022-04-20 12:01:24 +0000

    adding installation id as a metric label

--- workflowJobMetrics.go
+++ workflowJobMetrics.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/google/go-github/v43/github"
@@ -25,7 +26,7 @@ func NewWorkflowJobMetrics(cache IWorkflowNameCache) *WorkflowJobMetrics {
 }
 
 func (m WorkflowJobMetrics) log(eventType string, event *github.WorkflowJobEvent) {
-	log.Printf("reporting %s event with action %s for %s/%s on %s\n", eventType, event.GetAction(), event.GetOrg().GetLogin(), event.GetRepo().GetName(), event.GetWorkflowJob().GetName())
+	log.Printf("reporting %s event with action %s for %s/%s on %s for installation %d\n", eventType, event.GetAction(), event.GetOrg().GetLogin(), event.GetRepo().GetName(), event.GetWorkflowJob().GetName(), event.GetInstallation().GetID())
 }
 
 func (m WorkflowJobMetrics) report(eventType string, event *github.WorkflowJobEvent) {
@@ -37,15 +38,17 @@ func (m WorkflowJobMetrics) report(eventType string, event *github.WorkflowJobEv
 		workflowName = "unknown"
 	}
 
+	installationID := fmt.Sprint(event.GetInstallation().GetID())
+
 	actionCounter, found := m.getCounter(eventType, event.GetAction())
 	if found {
-		actionCounter.WithLabelValues(event.GetOrg().GetLogin(), event.GetRepo().GetName(), workflowName, event.GetWorkflowJob().GetName()).Inc()
+		actionCounter.WithLabelValues(event.GetOrg().GetLogin(), event.GetRepo().GetName(), workflowName, event.GetWorkflowJob().GetName(), installationID).Inc()
 	}
 
 	if event.GetAction() == "completed" {
 		conclusionCounter, found := m.getCounter(eventType, event.GetWorkflowJob().GetConclusion())
 		if found {
-			conclusionCounter.WithLabelValues(event.GetOrg().GetLogin(), event.GetRepo().GetName(), workflowName, event.GetWorkflowJob().GetName()).Inc()
+			conclusionCounter.WithLabelValues(event.GetOrg().GetLogin(), event.GetRepo().GetName(), workflowName, event.GetWorkflowJob().GetName(), installationID).Inc()
 		}
 
 		histogram, found := m.getHistogram(eventType, "duration")
@@ -54,7 +57,7 @@ func (m WorkflowJobMetrics) report(eventType string, event *github.WorkflowJobEv
 			start := event.GetWorkflowJob().GetStartedAt().Time
 			end := event.GetWorkflowJob().GetCompletedAt().Time
 
-			histogram.WithLabelValues(event.GetOrg().GetLogin(), event.GetRepo().GetName(), workflowName, event.GetWorkflowJob().GetName()).Observe(float64(end.Sub(start).Milliseconds()))
+			histogram.WithLabelValues(event.GetOrg().GetLogin(), event.GetRepo().GetName(), workflowName, event.GetWorkflowJob().GetName(), installationID).Observe(float64(end.Sub(start).Milliseconds()))
 		}
 	}
 }
@@ -67,7 +70,7 @@ func (m WorkflowJobMetrics) intializeHistograms() {
 		Help:      "The duration of workflow job, equivalent to the billing time",
 		Buckets:   prometheus.LinearBuckets(0, 2, 10),
 	},
-		[]string{"org", "repo", "workflow", "job"},
+		[]string{"org", "repo", "workflow", "job", "installation"},
 	)
 }
 
@@ -78,7 +81,7 @@ func (m WorkflowJobMetrics) intializeCounters() {
 		Name:      "queued",
 		Help:      "The total number of workflow jobs queued",
 	},
-		[]string{"org", "repo", "workflow", "job"},
+		[]string{"org", "repo", "workflow", "job", "installation"},
 	)
 
 	m.counters["github_actions_workflow_job_in_progress"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -87,7 +90,7 @@ func (m WorkflowJobMetrics) intializeCounters() {
 		Name:      "in_progress",
 		Help:      "The total number of workflow jobs in progress",
 	},
-		[]string{"org", "repo", "workflow", "job"},
+		[]string{"org", "repo", "workflow", "job", "installation"},
 	)
 
 	m.counters["github_actions_workflow_job_completed"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -96,7 +99,7 @@ func (m WorkflowJobMetrics) intializeCounters() {
 		Name:      "completed",
 		Help:      "The total number of workflow jobs completed",
 	},
-		[]string{"org", "repo", "workflow", "job"},
+		[]string{"org", "repo", "workflow", "job", "installation"},
 	)
 
 	m.counters["github_actions_workflow_job_success"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -105,7 +108,7 @@ func (m WorkflowJobMetrics) intializeCounters() {
 		Name:      "success",
 		Help:      "The total number of workflow runs with a 'success' conclusion",
 	},
-		[]string{"org", "repo", "workflow", "job"},
+		[]string{"org", "repo", "workflow", "job", "installation"},
 	)
 
 	m.counters["github_actions_workflow_job_cancelled"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -114,7 +117,7 @@ func (m WorkflowJobMetrics) intializeCounters() {
 		Name:      "cancelled",
 		Help:      "The total number of workflow runs with a 'cancelled' conclusion",
 	},
-		[]string{"org", "repo", "workflow", "job"},
+		[]string{"org", "repo", "workflow", "job", "installation"},
 	)
 
 	m.counters["github_actions_workflow_job_action_required"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -123,7 +126,7 @@ func (m WorkflowJobMetrics) intializeCounters() {
 		Name:      "action_required",
 		Help:      "The total number of workflow runs with a 'action_required' conclusion",
 	},
-		[]string{"org", "repo", "workflow", "job"},
+		[]string{"org", "repo", "workflow", "job", "installation"},
 	)
 
 	m.counters["github_actions_workflow_job_timed_out"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -132,7 +135,7 @@ func (m WorkflowJobMetrics) intializeCounters() {
 		Name:      "timed_out",
 		Help:      "The total number of workflow runs with a 'timed_out' conclusion",
 	},
-		[]string{"org", "repo", "workflow", "job"},
+		[]string{"org", "repo", "workflow", "job", "installation"},
 	)
 
 	m.counters["github_actions_workflow_job_failure"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -141,7 +144,7 @@ func (m WorkflowJobMetrics) intializeCounters() {
 		Name:      "failure",
 		Help:      "The total number of workflow runs with a 'failure' conclusion",
 	},
-		[]string{"org", "repo", "workflow", "job"},
+		[]string{"org", "repo", "workflow", "job", "installation"},
 	)
 
 	m.counters["github_actions_workflow_job_neutral"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -150,7 +153,7 @@ func (m WorkflowJobMetrics) intializeCounters() {
 		Name:      "neutral",
 		Help:      "The total number of workflow runs with a 'neutral' conclusion",
 	},
-		[]string{"org", "repo", "workflow", "job"},
+		[]string{"org", "repo", "workflow", "job", "installation"},
 	)
 
 	m.counters["github_actions_workflow_job_skipped"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -159,7 +162,7 @@ func (m WorkflowJobMetrics) intializeCounters() {
 		Name:      "skipped",
 		Help:      "The total number of workflow runs with a 'skipped' conclusion",
 	},
-		[]string{"org", "repo", "workflow", "job"},
+		[]string{"org", "repo", "workflow", "job", "installation"},
 	)
 
 	m.counters["github_actions_workflow_job_startup_failure"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -168,7 +171,7 @@ func (m WorkflowJobMetrics) intializeCounters() {
 		Name:      "startup_failure",
 		Help:      "The total number of workflow runs with a 'startup_failure' conclusion",
 	},
-		[]string{"org", "repo", "workflow", "job"},
+		[]string{"org", "repo", "workflow", "job", "installation"},
 	)
 
 	m.counters["github_actions_workflow_job_stale"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -177,6 +180,6 @@ func (m WorkflowJobMetrics) intializeCounters() {
 		Name:      "stale",
 		Help:      "The total number of workflow runs with a 'stale' conclusion",
 	},
-		[]string{"org", "repo", "workflow", "job"},
+		[]string{"org", "repo", "workflow", "job", "installation"},
 	)
 }
--- workflowRunMetrics.go
+++ workflowRunMetrics.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/google/go-github/v43/github"
@@ -25,22 +26,24 @@ func NewWorkflowRunMetrics(cache IWorkflowNameCache) *WorkflowRunMetrics {
 }
 
 func (m WorkflowRunMetrics) log(eventType string, event *github.WorkflowRunEvent) {
-	log.Printf("reporting %s event with action %s for %s/%s on %s\n", eventType, event.GetAction(), event.GetOrg().GetLogin(), event.GetRepo().GetName(), event.GetWorkflow().GetName())
+	log.Printf("reporting %s event with action %s for %s/%s on %s for installation %d\n", eventType, event.GetAction(), event.GetOrg().GetLogin(), event.GetRepo().GetName(), event.GetWorkflow().GetName(), event.GetInstallation().GetID())
 }
 
 func (m WorkflowRunMetrics) report(eventType string, event *github.WorkflowRunEvent) {
 	m.log(eventType, event)
 	actionCounter, found := m.getCounter(eventType, event.GetAction())
 
+	installationID := fmt.Sprint(event.GetInstallation().GetID())
+
 	if found {
 		m.cache.set(event)
-		actionCounter.WithLabelValues(event.GetOrg().GetLogin(), event.GetRepo().GetName(), event.GetWorkflow().GetName()).Inc()
+		actionCounter.WithLabelValues(event.GetOrg().GetLogin(), event.GetRepo().GetName(), event.GetWorkflow().GetName(), installationID).Inc()
 	}
 
 	if event.GetAction() == "completed" {
 		conclusionCounter, found := m.getCounter(eventType, event.GetWorkflowRun().GetConclusion())
 		if found {
-			conclusionCounter.WithLabelValues(event.GetOrg().GetLogin(), event.GetRepo().GetName(), event.GetWorkflow().GetName()).Inc()
+			conclusionCounter.WithLabelValues(event.GetOrg().GetLogin(), event.GetRepo().GetName(), event.GetWorkflow().GetName(), installationID).Inc()
 		}
 
 		histogram, found := m.getHistogram(eventType, "duration")
@@ -49,7 +52,7 @@ func (m WorkflowRunMetrics) report(eventType string, event *github.WorkflowRunEv
 			start := event.GetWorkflowRun().GetCreatedAt().Time
 			end := event.GetWorkflowRun().GetUpdatedAt().Time
 
-			histogram.WithLabelValues(event.GetOrg().GetLogin(), event.GetRepo().GetName(), event.GetWorkflow().GetName()).Observe(float64(end.Sub(start).Milliseconds()))
+			histogram.WithLabelValues(event.GetOrg().GetLogin(), event.GetRepo().GetName(), event.GetWorkflow().GetName(), installationID).Observe(float64(end.Sub(start).Milliseconds()))
 		}
 	}
 }
@@ -62,7 +65,7 @@ func (m WorkflowRunMetrics) intializeHistograms() {
 		Help:      "The duration of workflow runs",
 		Buckets:   prometheus.LinearBuckets(0, 2, 10),
 	},
-		[]string{"org", "repo", "workflow"},
+		[]string{"org", "repo", "workflow", "installation"},
 	)
 }
 
@@ -73,7 +76,7 @@ func (m WorkflowRunMetrics) intializeCounters() {
 		Name:      "requested",
 		Help:      "The total number of workflow runs requested",
 	},
-		[]string{"org", "repo", "workflow"},
+		[]string{"org", "repo", "workflow", "installation"},
 	)
 
 	m.counters["github_actions_workflow_run_completed"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -82,7 +85,7 @@ func (m WorkflowRunMetrics) intializeCounters() {
 		Name:      "completed",
 		Help:      "The total number of workflow runs completed",
 	},
-		[]string{"org", "repo", "workflow"},
+		[]string{"org", "repo", "workflow", "installation"},
 	)
 
 	m.counters["github_actions_workflow_run_success"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -91,7 +94,7 @@ func (m WorkflowRunMetrics) intializeCounters() {
 		Name:      "success",
 		Help:      "The total number of workflow runs with a 'success' conclusion",
 	},
-		[]string{"org", "repo", "workflow"},
+		[]string{"org", "repo", "workflow", "installation"},
 	)
 
 	m.counters["github_actions_workflow_run_cancelled"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -100,7 +103,7 @@ func (m WorkflowRunMetrics) intializeCounters() {
 		Name:      "cancelled",
 		Help:      "The total number of workflow runs with a 'cancelled' conclusion",
 	},
-		[]string{"org", "repo", "workflow"},
+		[]string{"org", "repo", "workflow", "installation"},
 	)
 
 	m.counters["github_actions_workflow_run_action_required"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -109,7 +112,7 @@ func (m WorkflowRunMetrics) intializeCounters() {
 		Name:      "action_required",
 		Help:      "The total number of workflow runs with a 'action_required' conclusion",
 	},
-		[]string{"org", "repo", "workflow"},
+		[]string{"org", "repo", "workflow", "installation"},
 	)
 
 	m.counters["github_actions_workflow_run_timed_out"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -118,7 +121,7 @@ func (m WorkflowRunMetrics) intializeCounters() {
 		Name:      "timed_out",
 		Help:      "The total number of workflow runs with a 'timed_out' conclusion",
 	},
-		[]string{"org", "repo", "workflow"},
+		[]string{"org", "repo", "workflow", "installation"},
 	)
 
 	m.counters["github_actions_workflow_run_failure"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -127,7 +130,7 @@ func (m WorkflowRunMetrics) intializeCounters() {
 		Name:      "failure",
 		Help:      "The total number of workflow runs with a 'failure' conclusion",
 	},
-		[]string{"org", "repo", "workflow"},
+		[]string{"org", "repo", "workflow", "installation"},
 	)
 
 	m.counters["github_actions_workflow_run_neutral"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -136,7 +139,7 @@ func (m WorkflowRunMetrics) intializeCounters() {
 		Name:      "neutral",
 		Help:      "The total number of workflow runs with a 'neutral' conclusion",
 	},
-		[]string{"org", "repo", "workflow"},
+		[]string{"org", "repo", "workflow", "installation"},
 	)
 
 	m.counters["github_actions_workflow_run_skipped"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -145,7 +148,7 @@ func (m WorkflowRunMetrics) intializeCounters() {
 		Name:      "skipped",
 		Help:      "The total number of workflow runs with a 'skipped' conclusion",
 	},
-		[]string{"org", "repo", "workflow"},
+		[]string{"org", "repo", "workflow", "installation"},
 	)
 
 	m.counters["github_actions_workflow_run_startup_failure"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -154,7 +157,7 @@ func (m WorkflowRunMetrics) intializeCounters() {
 		Name:      "startup_failure",
 		Help:      "The total number of workflow runs with a 'startup_failure' conclusion",
 	},
-		[]string{"org", "repo", "workflow"},
+		[]string{"org", "repo", "workflow", "installation"},
 	)
 
 	m.counters["github_actions_workflow_run_stale"] = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -163,6 +166,6 @@ func (m WorkflowRunMetrics) intializeCounters() {
 		Name:      "stale",
 		Help:      "The total number of workflow runs with a 'stale' conclusion",
 	},
-		[]string{"org", "repo", "workflow"},
+		[]string{"org", "repo", "workflow", "installation"},
 	)
 }
